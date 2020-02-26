62a725dedb48915cba50398bc8e583fe683fa828
M-Hagras <mohamed.hagras@pixelogicmedia.com>
2020-02-26 11:12:53 +0200

    adding state hooks

--- src/product.js
+++ src/product.js
@@ -1,26 +1,24 @@
 import React from "react";
 import {Table} from 'semantic-ui-react'
 
-class Product extends React.Component {
-	render() {
-		return (
-			<Table.Row>
-				<Table.Cell>
-					{this.props.product.name}
-				</Table.Cell>
-				<Table.Cell>
-					{this.props.product.price}
-				</Table.Cell>
-				<Table.Cell>
-					{this.props.product.department.name}
-				</Table.Cell>
-				<Table.Cell>
-					{this.props.product.promotions.map(p => p.code + '\n')}
-				</Table.Cell>
+function Product(props) {
+	return (
+		<Table.Row>
+			<Table.Cell>
+				{props.product.name}
+			</Table.Cell>
+			<Table.Cell>
+				{props.product.price}
+			</Table.Cell>
+			<Table.Cell>
+				{props.product.department.name}
+			</Table.Cell>
+			<Table.Cell>
+				{props.product.promotions.map(p => p.code + '\n')}
+			</Table.Cell>
 
-			</Table.Row>
-		)
-	}
+		</Table.Row>
+	)
 }
 
 export default Product;
--- src/searchable_products.js
+++ src/searchable_products.js
@@ -1,102 +1,96 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import {Pagination, Container, Grid, Input} from 'semantic-ui-react'
 import 'semantic-ui-css/semantic.min.css'
 import Products from './products';
 import DepartmentDropdown from './departments_dropdown'
 import PromotionsDropdown from "./promotionss_dropdown";
 
-class SearchableProducts extends React.Component {
-	state = {
-		products: [],
-		pages: 1,
-		page: 1,
-		departments: [],
-		promotions: [],
-		filters: {
-			promotion: '',
-			department: '',
-			product_text: ''
-		}
-	}
+function SearchableProducts() {
+	const [products, setProducts] = useState([]);
+	const [departments, setDepartments] = useState([]);
+	const [promotions, setPromotions] = useState([]);
+	const [pages, setPages] = useState(1);
+	const [page, setPage] = useState(1);
+	const [filters, setFilters] = useState({
+		promotion: '',
+		department: '',
+		product_text: ''
+	});
 
-	componentDidMount() {
-		this.update_products(this.state.filters, '1')
+	useEffect(() => {
+		update_products(filters, '1')
 		fetch('http://localhost:3000/v1/departments').then(res =>
-			res.json()).then(this.loadDepartments)
+			res.json()).then(loadDepartments)
 		fetch('http://localhost:3000/v1/promotions').then(res =>
-			res.json()).then(this.loadPromotions)
+			res.json()).then(loadPromotions)
+	}, [])
+
+	function loadProducts(res) {
+		setPage(res.page)
+		setPages(res.pages)
+		setProducts(res.products)
 	}
 
-	loadProducts = (res) => {
-		this.setState({...this.state, products: res.products, pages: res.pages, page: res.page})
+	function loadDepartments(res) {
+		setDepartments(res)
 	}
 
-	loadDepartments = (res) => {
-		this.setState({...this.state, departments: res})
+	function loadPromotions(res) {
+		setPromotions(res)
 	}
 
-	loadPromotions = (res) => {
-		this.setState({...this.state, promotions: res})
+	function handleDepartmentChangeSelection(e, data) {
+		filters.department = data.value
+		update_products(filters, '1')
 	}
 
-	handleDepartmentChangeSelection = (e, data) => {
-		let new_filters = this.state.filters
-		new_filters.department = data.value
-		this.update_products(new_filters, '1')
+	function handlePromotionsChangeSelection(e, data) {
+		filters.promotion = data.value
+		update_products(filters, '1')
 	}
 
-	handlePromotionsChangeSelection = (e, data) => {
-		let new_filters = this.state.filters
-		new_filters.promotion = data.value
-		this.update_products(new_filters, '1')
+	function handleProductTextSearch(e, data) {
+		filters.product_text = data.value
+		update_products(filters, '1')
 	}
 
-	handleProductTextSearch = (e, data) => {
-		let new_filters = this.state.filters
-		new_filters.product_text = data.value
-		this.update_products(new_filters, '1')
-	}
-
-	update_products = (new_filters, page) => {
-		this.setState({...this.state, filters: new_filters})
-		fetch('http://localhost:3000/v1/products/search?page=1&department_id=' + new_filters.department +
+	function update_products(new_filters, page) {
+		setFilters(new_filters)
+		fetch('http://localhost:3000/v1/products/search_products?page=1&department_id=' + new_filters.department +
 			'&promotion_id=' + new_filters.promotion + '&product_text=' + new_filters.product_text + '&page=' + page).then(res =>
-			res.json()).then(this.loadProducts)
+			res.json()).then(loadProducts)
 	}
 
-	handlePage =
-		(e, {activePage}) => {
+	function handlePage(e, {activePage}) {
 		let pageString = activePage.toString()
-		this.update_products(this.state.filters, pageString)
+		update_products(filters, pageString)
 	}
 
-	render() {
-		return (
-			<Container>
-				<Grid>
-					<Grid.Row>
-						<Grid.Column width={9}>
-							<Input fluid onChange={this.handleProductTextSearch} placeholder='Search...'/>
-						</Grid.Column>
-						<Grid.Column>
-							<DepartmentDropdown handleDepartmentChangeSelection={this.handleDepartmentChangeSelection}
-												departments={this.state.departments}/>
-						</Grid.Column>
-						<Grid.Column>
-							<PromotionsDropdown handlePromotionsChangeSelection={this.handlePromotionsChangeSelection}
-												promotions={this.state.promotions}/>
-						</Grid.Column>
-					</Grid.Row>
-				</Grid>
+	return (
+		<Container>
+			<Grid>
+				<Grid.Row>
+					<Grid.Column width={9}>
+						<Input fluid onChange={handleProductTextSearch} placeholder='Search...'/>
+					</Grid.Column>
+					<Grid.Column>
+						<DepartmentDropdown handleDepartmentChangeSelection={handleDepartmentChangeSelection}
+											departments={departments}/>
+					</Grid.Column>
+					<Grid.Column>
+						<PromotionsDropdown handlePromotionsChangeSelection={handlePromotionsChangeSelection}
+											promotions={promotions}/>
+					</Grid.Column>
+				</Grid.Row>
+			</Grid>
 
-				<Products products={this.state.products || []}/>
+			<Products products={products || []}/>
 
-				<Pagination onPageChange={this.handlePage} size='mini' siblingRange="6"
-							defaultActivePage={this.state.page}
-							totalPages={this.state.pages}/>
-			</Container>
-		)
-	}
+			<Pagination onPageChange={handlePage} class={'fluid'} size='mini' siblingRange="6"
+						defaultActivePage={page}
+						totalPages={pages}/>
+		</Container>
+	)
 }
 
 export default SearchableProducts;
